36e909b5591e7cef4fbc8010865896d0d62c68ce
CF-SZ6 <kaitomx@gmail.com>
2025-07-21 19:35:48 +0900

    step27 completed

--- steps/step27.py
+++ steps/step27.py
@@ -2,6 +2,8 @@ if '__file__' in globals():
     import os
     import sys
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import math
 import numpy as np
 from dezero import Variable
 from dezero import Function
@@ -21,9 +23,22 @@ class Sin(Function):
 def sin(x: Variable) -> Variable:
     return Sin()(x)
 
+def my_sin(x: Variable, threshold: float = 1e-7):
+    y = 0
+    for i in range(100000):
+        c = (-1) ** i / math.factorial(2 * i + 1)
+        t = c * x ** (2 * i + 1)
+        y = y + t
+        if abs(t.data) < threshold:
+            break
+    return y
+
 x = Variable(np.array(np.pi / 4))
-y = sin(x)
+y = my_sin(x)
 y.backward()
 
-print(x.data)
-print(y.grad)
+print(y.data)
+print(x.grad)
+x.name = 'x'
+y.name = 'y'
+plot_dot_graph(y, verbose=False, to_file='my_sin.png')
